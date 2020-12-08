9dd0cf7b610804e752d0e23d5f4c25373ea0e30c
Matthew Hamill <matthewhamill@ntlworld.com>
2020-12-08 00:24:33 +0000

    day7

--- day7/BagSort.java
+++ day7/BagSort.java
@@ -3,6 +3,12 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.stream.Stream;
 
 public class BagSort {
     private static ArrayList<String> readData(String filename) {
@@ -21,6 +27,21 @@ public class BagSort {
         return fileData;
     }
 
+    public static Map<String, String> readToMap(String filename) {
+        String delimiter = " bags contain ";
+        Map<String, String> map = new HashMap<>();
+
+        try(Stream<String> lines = Files.lines(Paths.get(filename))){
+            lines.forEach(
+                line -> map.putIfAbsent(line.split(delimiter)[0], line.split(delimiter)[1])
+            );
+        } catch (IOException e) {
+            System.out.println("Oh no");
+        }
+
+        return map;    
+    }
+
     private static ArrayList<String> getParentBags(String colour, ArrayList<String> bags) {
         ArrayList<String> parentBags = new ArrayList<>();
         Pattern p = Pattern.compile("(\\w+\\s\\w+).*" + colour + ".*");
@@ -35,44 +56,49 @@ public class BagSort {
 
     private static ArrayList<String> getAncestorBags(String colour, ArrayList<String> bags, ArrayList<String> ancestors) {
         ArrayList<String> parentBags = getParentBags(colour, bags);
-        if (parentBags.size() > 0) {
-            for (String bag : parentBags) {
-                if (!ancestors.contains(bag)) {
-                    ancestors.add(bag);
-                }
-                getAncestorBags(bag, bags, ancestors);
+        for (String bag : parentBags) {
+            if (!ancestors.contains(bag)) {
+                ancestors.add(bag);
             }
+            getAncestorBags(bag, bags, ancestors);
         }
         return ancestors;
     }
 
-    private static int countChildren(String bag) {
-        int count = 0;
+    private static ArrayList<String> getChildBags(String colour, Map<String, String> bags) {
+        String[] children = bags.get(colour).split(", ");
+        Pattern p = Pattern.compile("\\d\\s(\\w+\\s\\w+)\\s\\w+\\W?");
+        ArrayList<String> childList = new ArrayList<>();
+        for (String bag : children) {
+            Matcher m = p.matcher(bag);
+            if (m.matches()) childList.add(m.group(1));
+        }
+        return childList;
+    }
+
+    private static int count(String bag) {
         for (char c : bag.toCharArray()) {
-            if (Character.isDigit(c)) {
-                count = count + Integer.parseInt(""+c);
-            }
+            if (Character.isDigit(c)) return Integer.parseInt(""+c);
+        }
+        return 0;
+    }
+
+    private static int getTotalCount(String colour, Map<String, String> bags) {
+        ArrayList<String> childBags = getChildBags(colour, bags);
+        int count = 0;
+        for (int i = 0; i < childBags.size(); i++) {
+            count += count(bags.get(colour).split(",")[i]) + count(bags.get(colour).split(",")[i]) * getTotalCount(childBags.get(i), bags);
         }
         return count;
     }
-
-    private static ArrayList<String> getChildBags(String colour, ArrayList<String> bags) {
-        Pattern p = Pattern.compile(colour + " bags contain((\\s\\d\\s\\w+\\s\\w+\\s\\w+\\W)+)")
-        String childString = "";
-        for (String bag : bags) {
-            Matcher m = p.matcher(bag);
-            if (p.matches()) {
-
-            }
-        }
-    }
-
-    private static ArrayList<String>
 
     public static void main(String[] args) {
         ArrayList<String> bags = readData("bags.txt");
         ArrayList<String> goldenAncestors = getAncestorBags("shiny gold", bags, new ArrayList<String>());
         System.out.println(goldenAncestors.size());
-        System.out.println(countChildren("muted lime bags contain 1 wavy lime bag, 1 vibrant green bag, 3 light yellow bags."));
+
+        Map<String, String> bagMap = readToMap("bags.txt");
+        int totalCount = getTotalCount("shiny gold", bagMap);
+        System.out.println(totalCount);
     }
 }
